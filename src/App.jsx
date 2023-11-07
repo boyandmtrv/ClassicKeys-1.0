702848db4644a7feb6c207048494ab66b7283de0
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-11-07 19:38:10 +0200

    Environment setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h2>Welcome to</h2>
+            <h1>Cliknie</h1>
+            <button>Get Started</button>
+        </>
+    )
+}
+
+export default App
