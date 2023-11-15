a4ff55e6edb67c7d839953c5d5591e73d256aeee
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-11-15 14:50:41 +0200

    Added refresh and re-take buttons, imported props, to be used later for the final score

--- src/components/game/EndGame.jsx
+++ src/components/game/EndGame.jsx
@@ -1,20 +1,29 @@
-import wordHelpers from '../../utils/wordUtils';
 import styles from './EndGame.module.css';
 
-const EndGame = () => {
+const EndGame = ({
+    correctWord,
+    incorrectWord,
+    onRefresh,
+    onRetake
+}) => {
     return (
         <div className={styles.EndGame}>
             <div className={styles.endResults}>
                 <div className={styles.correntResult}>
                     <p>Correct Words:</p>
+                    <p>{correctWord}</p>
                 </div>
                 <div className={styles.incorrentResult}>
                     <p>Incorrect Words:</p>
-
+                    <p>{incorrectWord}</p>
                 </div>
                 <div className={styles.accuracyResult}>
                     <p>Accuracy:</p>
-
+                    {Math.round((correctWord / (correctWord + incorrectWord)) * 100)}%
+                </div>
+                <div className="endGameButtons">
+                    <button onClick={onRefresh}>Refresh</button>
+                    <button onClick={onRetake}>Re-take Test</button>
                 </div>
             </div>
         </div>
