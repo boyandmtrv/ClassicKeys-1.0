cbb80c7506de93583c4254977f2a19e7127af17f
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-11-17 12:55:03 +0200

    Endgame component styling added

--- src/components/game/EndGame.jsx
+++ src/components/game/EndGame.jsx
@@ -1,5 +1,3 @@
-import styles from './EndGame.module.css';
-
 const EndGame = ({
     correctWord,
     incorrectWord,
@@ -7,27 +5,27 @@ const EndGame = ({
     onRetake
 }) => {
     return (
-        <div className={styles.EndGame}>
-            <div className={styles.endResults}>
-                <div className={styles.correntResult}>
+        <div className="flex flex-col items-center w-full h-full">
+            <div className="flex space-x-10 mb-4 mt-[-200px]" >
+                <div className="w-72 h-72 border-4 border-amber-300 rounded-full flex flex-col items-center justify-center text-white text-2xl">
                     <p>Correct Words:</p>
                     <p>{correctWord}</p>
                 </div>
-                <div className={styles.incorrentResult}>
+                <div className="w-72 h-72 border-4 border-amber-300 rounded-full flex flex-col items-center justify-center text-white text-2xl">
                     <p>Incorrect Words:</p>
                     <p>{incorrectWord}</p>
                 </div>
-                <div className={styles.accuracyResult}>
+                <div className="w-72 h-72 border-4 border-amber-300  rounded-full flex flex-col items-center justify-center text-white text-2xl">
                     <p>Accuracy:</p>
                     {Math.round((correctWord / (correctWord + incorrectWord)) * 100)}%
                 </div>
-                <div className="endGameButtons">
-                    <button onClick={onRefresh}>Refresh</button>
-                    <button onClick={onRetake}>Re-take Test</button>
-                </div>
+            </div>
+            <div className="flex space-x-4 mt-10">
+                <button className="w-48 h-16 px-4 py-2 border-4 border-amber-50 rounded-full text-white text-xl" onClick={onRefresh}>Refresh</button>
+                <button className="w-48 h-16 only:px-4 py-2 border-4 border-amber-50 rounded-full text-white text-xl" onClick={onRetake}>Re-take Test</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default EndGame;
