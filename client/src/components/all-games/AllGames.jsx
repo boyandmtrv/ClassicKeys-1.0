3424f7ebee0f02eb2e820b9e75454f616d5e2d3a
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-11-22 13:36:48 +0200

    Each game dyncamically added and service for it created

--- client/src/components/all-games/AllGames.jsx
+++ client/src/components/all-games/AllGames.jsx
@@ -1,114 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import * as gameService from '../../services/gameService';
+import EachGame from "./each-game/EachGame";
 
 const AllGames = () => {
+
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => setGames(result));
+    }, []);
+
+    console.log(games);
+
     return (
         <div className="flex flex-wrap mt-[-112px] justify-start min-h-screen flex-1 px-20 bg-zinc-800 pt-[150px] gap-10 ml-5">
 
-            <div className="bg-zinc-700 p-10 w-[25rem] h-[22rem] border-2 border-black rounded-md border-b-8 border-l-8">
-                <div className="flex font-light text-xl underline mb-5">
-                    <svg className="h-6 w-6 rotate-90 -ml-2" stroke="#fcd34d">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M22 15H4" />
-                    </svg>
-                    <p>Difficulty level</p>
-                </div>
+          {games.map(game => (
+            <EachGame key={game._id} {...game}/>
+          ))}
 
-                <h2 className="font-bold text-3xl mt-2 text-[#D1D0C5]">
-                    Rapid Event Notification System at Netflix
-                </h2>
-
-                <p className="mt-5 text-white text-xl">
-                    By: <br />
-                    Game Creator: Username
-                </p>
-
-                <button className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl mt-7 inline-flex items-center group border-2 border-black rounded-md border-b-4 border-l-4 ">
-                    Check it
-                </button>
-            </div>
-            <div className="bg-zinc-700 p-10 w-[25rem] h-[22rem] border-2 border-black rounded-md border-b-8 border-l-8">
-                <div className="flex font-light text-xl underline mb-5">
-                    <svg className="h-6 w-6 rotate-90 -ml-2" stroke="#fcd34d">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M22 15H4" />
-                    </svg>
-                    <p>Difficulty level</p>
-                </div>
-
-                <h2 className="font-bold text-3xl mt-2 text-[#D1D0C5]">
-                    Rapid Event Notification System at Netflix
-                </h2>
-
-                <p className="mt-5 text-white text-xl">
-                    By: <br />
-                    Game Creator: Username
-                </p>
-
-                <button className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl mt-7 inline-flex items-center group border-2 border-black rounded-md border-b-4 border-l-4 ">
-                    Check it
-                </button>
-            </div>
-            <div className="bg-zinc-700 p-10 w-[25rem] h-[22rem] border-2 border-black rounded-md border-b-8 border-l-8">
-                <div className="flex font-light text-xl underline mb-5">
-                    <svg className="h-6 w-6 rotate-90 -ml-2" stroke="#fcd34d">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M22 15H4" />
-                    </svg>
-                    <p>Difficulty level</p>
-                </div>
-
-                <h2 className="font-bold text-3xl mt-2 text-[#D1D0C5]">
-                    Rapid Event Notification System at Netflix
-                </h2>
-
-                <p className="mt-5 text-white text-xl">
-                    By: <br />
-                    Game Creator: Username
-                </p>
-
-                <button className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl mt-7 inline-flex items-center group border-2 border-black rounded-md border-b-4 border-l-4 ">
-                    Check it
-                </button>
-            </div>
-            <div className="bg-zinc-700 p-10 w-[25rem] h-[22rem] border-2 border-black rounded-md border-b-8 border-l-8">
-                <div className="flex font-light text-xl underline mb-5">
-                    <svg className="h-6 w-6 rotate-90 -ml-2" stroke="#fcd34d">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M22 15H4" />
-                    </svg>
-                    <p>Difficulty level</p>
-                </div>
-
-                <h2 className="font-bold text-3xl mt-2 text-[#D1D0C5]">
-                    Rapid Event Notification System at Netflix
-                </h2>
-
-                <p className="mt-5 text-white text-xl">
-                    By: <br />
-                    Game Creator: Username
-                </p>
-
-                <button className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl mt-7 inline-flex items-center group border-2 border-black rounded-md border-b-4 border-l-4 ">
-                    Check it
-                </button>
-            </div>
-            <div className="bg-zinc-700 p-10 w-[25rem] h-[22rem] border-2 border-black rounded-md border-b-8 border-l-8">
-                <div className="flex font-light text-xl underline mb-5">
-                    <svg className="h-6 w-6 rotate-90 -ml-2" stroke="#fcd34d">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M22 15H4" />
-                    </svg>
-                    <p>Difficulty level</p>
-                </div>
-
-                <h2 className="font-bold text-3xl mt-2 text-[#D1D0C5]">
-                    Rapid Event Notification System at Netflix
-                </h2>
-
-                <p className="mt-5 text-white text-xl">
-                    By: <br />
-                    Game Creator: Username
-                </p>
-
-                <button className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl mt-7 inline-flex items-center group border-2 border-black rounded-md border-b-4 border-l-4 ">
-                    Check it
-                </button>
-            </div>
+          {games.length === 0 && <p>No games added</p>}
+        
 
         </div>
     )
---
+++ client/src/components/all-games/each-game/EachGame.jsx
@@ -0,0 +1,28 @@
+const EachGame = ({ difficulty, title }) => {
+    return (
+      <div className="bg-zinc-700 p-10 w-[25rem] h-[22rem] border-2 border-black rounded-md border-b-8 border-l-8">
+        <div className="flex font-light text-xl underline mb-5">
+          <svg className="h-6 w-6 rotate-90 -ml-2" stroke="#fcd34d">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M22 15H4" />
+          </svg>
+          <p>Difficulty level: {difficulty}</p>
+        </div>
+  
+        <h2 className="font-bold text-3xl mt-2 text-[#D1D0C5] truncate">
+          {title}
+        </h2>
+  
+        <p className="mt-5 text-white text-xl">
+          By: <br />
+          Game Creator: Username
+        </p>
+  
+        <button className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl mt-7 inline-flex items-center group border-2 border-black rounded-md border-b-4 border-l-4 ">
+          Check it
+        </button>
+      </div>
+    );
+  };
+  
+  export default EachGame;
+  
