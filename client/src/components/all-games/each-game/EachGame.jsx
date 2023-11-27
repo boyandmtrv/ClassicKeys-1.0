0858c03846a5a5b3cb396143707e124bd6f12199
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-11-27 19:14:17 +0200

    Each and All games components reworked

--- client/src/components/all-games/each-game/EachGame.jsx
+++ client/src/components/all-games/each-game/EachGame.jsx
@@ -3,42 +3,43 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../../contexts/AuthContext';
 
 const EachGame = ({
-  _id,
-  difficulty,
-  title
+    _id,
+    difficulty,
+    title
 }) => {
-  const { username } = useContext(AuthContext);
+    const { username } = useContext(AuthContext);
 
-  return (
-    <div className="bg-zinc-700 p-10 w-full h-32 border-2 border-black rounded-md border-b-8 border-l-8 flex flex-row flex-wrap justify-between items-center text-white mb-4">
-      <div className="flex flex-row">
-        <div className="font-light text-xl">
-          <p>Difficulty level: {difficulty}</p>
-        </div>
-        <h2 className="font-bold text-xl text-[#D1D0C5]">
-          Title: {title}
-        </h2>
-        <p className="text-white text-xl">
-          Game Creator: {username}
-        </p>
-      </div>
 
-      <div className="flex flex-row items-center justify-center space-x-4">
-        <Link
-          to={`/games/${_id}/details`}
-          className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl group border-2 border-black rounded-md border-b-4 border-l-4"
-        >
-          Check details
-        </Link>
-        <Link
-          to={`/games/${_id}/details`}
-          className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl group border-2 border-black rounded-md border-b-4 border-l-4"
-        >
-          Play
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <tr className="border-b border-zinc-700 bg-zinc-800 text-xl">
+            <td className="p-3 px-5">
+                <p>{title}</p>
+            </td>
+            <td className="p-3 px-5">
+                <p>{difficulty}</p>
+            </td>
+            <td className="p-3 px-5">
+                <p>{username}</p>
+            </td>
+            <td className="p-3 px-5 flex justify-end">
+                <Link
+                    to={`/games/${_id}/details`}
+                    className="bg-zinc-800 text-[#D1D0C5] mr-5 py-1 px-4 text-2xl group border-2 border-black rounded-md border-b-4 border-l-4 hover:bg-amber-300 hover:text-black transition duration-300 ease-in-out transform"
+                > Details
+                </Link>
+                <Link
+                    to={`/games/${_id}/details`}
+                    className="bg-amber-300 text-black font-semibold py-1 px-4 text-2xl group border-2 border-black rounded-md border-b-4 border-l-4 hover:bg-zinc-800 hover:text-[#D1D0C5] transition duration-300 ease-in-out transform"
+                >
+                    Play
+                </Link>
+            </td>
+        </tr>
+
+    );
 };
 
 export default EachGame;
+
+
+
