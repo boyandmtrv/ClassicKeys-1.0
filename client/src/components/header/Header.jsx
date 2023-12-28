9c0d29656dc94507b0efd30a42c15362d8f9b9fb
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-28 14:28:42 +0200

    minor changes,  brackets added by mistake removed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import  Nav  from "./Nav";
 
 const Header = () => {
     return (
-        <header className="bg-transparent absolute z-[20] flex-wrap mx-auto flex w-full justify-between border-white p-6 ">
+        <header className="bg-transparent absolute z-[20] flex-wrap mx-auto flex w-full justify-between border-white p-6">
             <Nav />
         </header>
     );
--- client/src/components/header/Nav.jsx
+++ client/src/components/header/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
         <nav className="w-16 h-16 text-center flex justify-center items-center border-2 border-black rounded-md border-b-4 border-l-4 relative bg-transparent">
             <div>
                 <button
-                    className=" text-[#D1D0C5]"
+                    className="text-[#D1D0C5]"
                     onClick={toggleNav}>
                     {isOpen
                         ? <ArrowBigUpDash className="w-[40px] h-[40px]" />
@@ -26,7 +26,7 @@ const Nav = () => {
                 <div className="flex flex-col items-center mt-[168px] basis-full text-amber-200 text-2xl space-y-5 absolute top-full bg-transparent">
                     <NavLinks />
                 </div>
-            )};
+            )}
         </nav>
     );
 };
