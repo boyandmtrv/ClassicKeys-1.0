f3eaf2d786913abb604b354faba9e6597dbaeeb1
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-29 10:36:48 +0200

    Responsive AllGames and EachGame components finished

--- client/src/components/all-games/AllGames.jsx
+++ client/src/components/all-games/AllGames.jsx
@@ -17,14 +17,14 @@ const AllGames = () => {
     return (
         <div className="text-gray-900 backdrop-blur  w-full h-screen">
             <div className="p-4 flex justify-center">
-                <h1 className="text-5xl text-amber-300 mt-[5%]">
+                <h1 className="lg:text-5xl md:text-4xl sm:text-3xl min-[320px]:text-2xl text-amber-300 lg:mt-[5%] md:mt-[5%] sm:mt-[10%] min-[320px]:mt-[20%]">
                     All Created Games
                 </h1>
             </div>
             <div className="px-3 py-4 flex justify-center">
                 <table className="w-full text-md bg-transparent text-white shadow-md rounded mb-4">
                     <tbody>
-                        <tr className="border-b border-zinc-700 text-2xl text-[#D1D0C5]">
+                        <tr className="border-b border-zinc-700 lg:text-xl md:text-md sm:text-sm min-[320px]:text-[11px] text-[#D1D0C5]">
                             <th
                                 className="text-left p-3 px-5">Game title
                             </th>
@@ -45,7 +45,7 @@ const AllGames = () => {
             </div>
             {games.length === 0 && (
                 <div className="flex flex-col justify-center items-center mt-[10%] m-[20%]">
-                    <p className="text-white text-6xl mb-4">
+                    <p className="text-white lg:text-6xl md:text-5xl sm:text-4xl mb-4">
                         No games added, would you like to be the first one?
                     </p>
                     <Link
--- client/src/components/all-games/each-game/EachGame.jsx
+++ client/src/components/all-games/each-game/EachGame.jsx
@@ -7,7 +7,7 @@ const EachGame = ({
     time
 }) => {
     return (
-        <tr className="border-b border-zinc-700 bg-transparent text-xl">
+        <tr className="border-b border-zinc-700 bg-transparent lg:text-xl md:text-md sm:text-sm min-[320px]:text-[12px]">
             <td className="p-3 px-5">
                 <p>{title}</p>
             </td>
@@ -20,7 +20,7 @@ const EachGame = ({
             <td className="p-3 px-5 flex justify-end">
                 <Link
                     to={`/games/${_id}/details`}
-                    className="bg-transparent text-[#D1D0C5] mr-5 py-1 px-4 text-2xl group border-2 border-black rounded-md border-b-4 border-l-4 hover:bg-amber-300 hover:text-black transition duration-300 ease-in-out transform"
+                    className="bg-transparent text-[#D1D0C5] ml-[-50px] mt-1 py-1 px-4 lg:text-2xl md:text-xl sm:text-md min-[320px]:text-[10px] group border-2 border-black rounded-md border-b-4 border-l-4 hover:bg-amber-300 hover:text-black transition duration-300 ease-in-out transform"
                 >
                     Details
                 </Link>
