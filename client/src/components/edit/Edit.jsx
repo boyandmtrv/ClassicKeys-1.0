67520b75623f0f72ddb2511bb932049c9f8b28c5
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-29 11:48:11 +0200

    Reworked edit component into responsive

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -49,12 +49,13 @@ const Edit = () => {
 
 
     return (
-        <div className="flex flex-col items-center w-full justify-center h-screen flex-1 px-20 text-center">
+        <div className="flex flex-col items-center w-full justify-center h-screen flex-1 px-20 text-center lg:ml-[0%] md:ml-[0%] sm:ml-[0%] min-[320px]:ml-[-25%]">
             <form
-                className="backdrop-blur shadow-2xl flex w-2/3 max-w-5xl  border-2 border-black rounded-md border-b-8 border-l-8"
+                className="backdrop-blur shadow-2xl flex lg:w-2/3 md:w-2/3 sm:w-4/5 min-[320px]:w-4/5 lg:max-w-5xl
+                md:max-w-3xl sm:max-w-2xl min-[320px]:max-w-xl  border-2 border-black rounded-md border-b-8 border-l-8"
                 onSubmit={editGameSubmitHandler}>
-                <div className="w-3/5 p-10">
-                    <div className="text-left font-bold text-5xl text-[#D1D0C5]">
+                <div className="lg:w-3/5 md:w-4/5 sm:w-3/5 p-10">
+                    <div className="text-left font-bold lg:text-4xl md:text-3xl sm:text-xl text-[#D1D0C5]">
                         <span
                             className="text-white">You are in
                         </span> editing mode, {username}
@@ -62,7 +63,7 @@ const Edit = () => {
                     </div>
                     <div className="flex flex-col items-center">
                         <textarea
-                            className="text-black bg-[#D1D0C5] border-2 border-black rounded-md border-b-4 border-l-4 text-3xl w-full h-80 p-2 mt-[50px] resize-none placeholder-black scrollbar scrollbar-thumb-gray-900 scrollbar-track-[#D1D0C5]"
+                            className="text-black bg-[#D1D0C5] border-2 border-black rounded-md border-b-4 border-l-4 lg:text-2xl md:text-2xl sm:text-xl min-[320px]:text-sm min-[320px]:h-60 w-full lg:h-80 md:h-60 sm:h-60 p-2 mt-[50px] resize-none placeholder-black scrollbar scrollbar-thumb-gray-900 scrollbar-track-[#D1D0C5]"
                             id="userText"
                             name="userText"
                             type="text"
@@ -72,12 +73,12 @@ const Edit = () => {
                         />
                     </div>
                     <Link to={`/games`}
-                        className="flex flex-col justify-center item border-2 border-black rounded-md border-b-4 border-l-4 w-24 h-12 font-black px-2 text-2xl text-[#D1D0C5] mt-8">
+                        className="flex flex-col justify-center item border-2 border-black rounded-md border-b-4 border-l-4 lg:w-48 md:w-48 sm:w-38 min-[320px]:w-24 h-12 font-black px-2 lg:text-md md:text-sm sm:text-[10px] min-[320px]:text-[10px] text-[#D1D0C5] mt-8">
                         Back
                     </Link>
                 </div>
-                <div className="w-2/5 bg-gradient-to-br from-zinc-800 to-zinc-900 text-[#D1D0C5] py-24 px-12">
-                    <div className="text-center font-bold text-4xl">
+                <div className="lg:w-2/5 md:w-3/5 sm:w-2/5 min-[320px]:w-[100%] bg-gradient-to-br from-zinc-800 to-zinc-900 text-[#D1D0C5] py-24 px-12">
+                    <div className="text-center font-bold lg:text-2xl md:text-xl sm:text-sm">
                         <span
                             className="text-white">
                             Change
@@ -85,7 +86,7 @@ const Edit = () => {
                     </div>
                     <div className="flex flex-col mt-11">
                         <input
-                            className="h-12 text-[#D1D0C5] border-2 border-black rounded-md border-b-4 border-l-4 bg-zinc-800 pl-2 placeholder-[#D1D0C5] text-2xl"
+                            className="h-12 lg:w-full md:w-48 sm:w-32 min-[320px]:w-32 text-[#D1D0C5] border-2 border-black rounded-md border-b-4 border-l-4 bg-zinc-800 pl-2 placeholder-[#D1D0C5] lg:text-md sm:text-sm lg:ml-[0%] md:ml-[0%] sm:ml-[-50%]  min-[320px]:ml-[-25%]"
                             type="text"
                             id="title"
                             name="title"
@@ -94,10 +95,10 @@ const Edit = () => {
                             placeholder="Game name"
                         />
                     </div>
-                    <p className="mt-7 text-xl">
+                    <p className="mt-7 lg:text-md">
                         Difficulty:
                     </p>
-                    <div className="text-black text-2xl font-normal justify-center flex gap-2 pt-2">
+                    <div className="text-black lg:text-xl font-normal justify-center flex gap-2 pt-2">
                         <select
                             className={`border-2 bg-zinc-800 border-black text-[#D1D0C5] rounded-md border-b-4 border-l-4 font-black px-2 w-36 `}
                             id="difficulty"
@@ -113,7 +114,7 @@ const Edit = () => {
                     <p className="mt-7">
                         Seconds:
                     </p>
-                    <div className="text-black text-2xl font-normal justify-center flex gap gap-2 pt-2">
+                    <div className="text-black lg:text-xl font-normal justify-center flex gap gap-2 pt-2">
                         <select
                             className={`border-2 bg-zinc-800 border-black text-[#D1D0C5] rounded-md border-b-4 border-l-4 font-black px-2 w-36 `}
                             id="time"
@@ -126,7 +127,7 @@ const Edit = () => {
                             <option value="30">30s</option>
                         </select>
                     </div>
-                    <div className="text-[#D1D0C5] text-2xl font-normal justify-center flex gap gap-2 pt-10">
+                    <div className="text-[#D1D0C5] lg:text-xl md:text-xl  sm:text-sm font-normal justify-center flex gap gap-2 pt-10">
                         <button
                             className="border-2 border-black rounded-md border-b-4 border-l-4 w-36 h-12 font-black px-2 mt-10 bg-amber-400 text-[#D1D0C5]"
                             type="submit"
