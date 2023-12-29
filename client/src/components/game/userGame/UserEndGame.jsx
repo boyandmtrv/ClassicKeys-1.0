771cd2d12395e107599f21cc553df072f427ad00
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-12-29 14:37:07 +0200

    UserEndGame Responsive design created

--- client/src/components/game/userGame/UserEndGame.jsx
+++ client/src/components/game/userGame/UserEndGame.jsx
@@ -56,14 +56,14 @@ const UserEndGame = ({
 
     return (
         <div className="w-full h-screen flex bg-transparent">
-            <div className="w-[50%] h-screen from-zinc-800 to-zinc-900">
+            <div className="lg:w-[50%] lg:h-screen md:lg:w-[50%] md:h-screen sm:w-[90%] min-[320px]:w-[90%] h-screen from-zinc-800 to-zinc-900">
                 <animated.h1
-                    className="translate-x-2/4 translate-y-[-50] mt-[25%] text-[120px] text-white"
+                    className="translate-x-2/4 translate-y-[-50] lg:mt-[25%] md:mt-[30%] sm:mt-[50%] min-[320px]:mt-[100%] lg:ml-[0%] md:ml-[0%] sm:ml-[50%] min-[320px]:ml-[20%] lg:text-[120px] md:text-[80px] sm:text-[55px] min-[320px]:text-[30px] text-white"
                     style={headerProps}>
                     <span className="text-black tracking-tighter">
                         <div className="text-white">{textToShow}, {username}</div>
                     </span>
-                    <div className="text-3xl ml-[180px] text-[#D1D0C5]">
+                    <div className="lg:text-3xl md:text-2xl sm:text-xl min-[320px]:text-sm lg:ml-[180px] md:ml-[50px] sm:ml-[30px] min-[320px]:ml-[5px] text-[#D1D0C5]">
                         <div className="flex space-x-4 mt-10">
                             <animated.button
                                 className={linkStyle}
@@ -75,30 +75,30 @@ const UserEndGame = ({
                     </div>
                 </animated.h1>
             </div>
-            <div className="w-[45%] h-screen from-zinc-800 to-zinc-900 flex flex-col justify-center items-center">
+            <div className="lg:w-[45%] md:w-[40%] sm:w-[40%] min-[320px]:w-[10%] h-screen from-zinc-800 to-zinc-900 flex flex-col justify-center items-center">
                 <animated.div
-                    className="w-full h-[75%] flex flex-col items-center justify-between mt-10 ml-[30%] text-gray-500 text-3xl space-y-4" style={statsProps}>
+                    className="w-full h-[75%] flex lg:flex-col md:flex-col sm:flex-row items-center justify-between mt-10 lg:ml-[700px] md:ml-[400px] sm:mr-[650px] min-[320px]:mr-[550px] lg:mt-[0%] md:mt-[0%] sm:mt-[100%] min-[320px]:mt-[200px] text-gray-500 lg:text-3xl md:text-2xl sm:text-2xl lg:space-y-4 md:space-y-4 sm:space-x-8 min-[320px]: space-x-4" style={statsProps}>
                     <div className="flex flex-col">
                         <p>Correct Words:</p>
-                        <p className="text-amber-300 text-9xl">
+                        <p className="text-amber-300 lg:text-9xl md:text-8xl">
                             {correctWord}
                         </p>
                     </div>
                     <div className="flex flex-col">
                         <p>Incorrect Words:</p>
-                        <p className="text-amber-300 text-9xl">
+                        <p className="text-amber-300 lg:text-9xl md:text-8xl">
                             {incorrectWord}
                         </p>
                     </div>
                     <div className="flex flex-col">
                         <p>Accuracy:</p>
-                        <p className="text-amber-300 text-9xl">
+                        <p className="text-amber-300 lg:text-9xl md:text-8xl">
                             {acc}%
                         </p>
                     </div>
                     <div className="flex flex-col">
                         <p>WPM:</p>
-                        <p className="text-amber-300 text-9xl">
+                        <p className="text-amber-300 lg:text-9xl md:text-8xl">
                             {wpm}
                         </p>
                     </div>
